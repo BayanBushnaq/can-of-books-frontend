c8ba2d0db9305126572796c0ea82a9b4ec49db6c
BayanBushnaq <bayanmohdali@yahoo.com>
2022-09-06 17:29:40 +0300

    lab11done

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,67 +1,65 @@
-import React from 'react';
-import Carousel from 'react-bootstrap/Carousel';
-import axios from 'axios'
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import axios from "axios";
 
 
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      books: []
-    }
-  }
-
+    this.state = {books: []};}
+      
+    
   
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
-  componentDidMount=()=>{
-    axios 
-      .get('http:localhost:3001/books')
-      .then((result)=>{
-        this.setState({books:result.data})
+  componentDidMount = () => {
+    axios
+      .get("http://localhost:3010/books")
+      .then((result) => {
+        this.setState({ books: result.data });
       })
-      .catch((err)=>{
-        console.log(err)
-      })
-  }
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   render() {
-
     /* TODO: render all the books in a Carousel */
+    return ( 
+      <>
+      {this.state.books.length > 0 ? (
+        <Carousel >
+          {this.state.books.map(item => {
+            return (
 
-    return (
-      <div  id="CarouselDiv">
-      {this.state.books.length ? (
-        <div id="secondaryDiv" style={{ width: "400px" }}>
-          <Carousel fade>
-            {this.state.books.map((item) => {
-              return(
               <Carousel.Item>
                 <img
                   className="d-block w-100"
-                  src="https://play-lh.googleusercontent.com/DmpYQrVcldrDuz5uyATqGbNvTALsJ1Bg3fpXM0p-VsRNM19osEB9-_ByvdjSbTvZQg=w450-h300-rw"
-                  alt="Slide"
+                  src="https://cdn2.teebooks.com/3568-large_default/wall-bookshelves-45-x-15-cm-set-of-2.jpg"
+                  alt="First slide"
                 />
-                <Carousel.Caption>
+                <Carousel.Caption style={{ color: "black" }}>
                   <h3>{item.title}</h3>
                   <p>{item.description}</p>
                   <p>{item.status}</p>
                 </Carousel.Caption>
               </Carousel.Item>
-              )
 
-            })}
-          </Carousel>
-        </div>
+            )
+          }
+          )}
+
+
+        </Carousel>
+
       ) : (
-        <h3>No Books Found :(</h3>
+        <h3>the book collection is empty.</h3>
       )}
-    </div>
-  );
-}
-}
 
 
 
+</>
+        ) 
+    }}
 
 export default BestBooks;
